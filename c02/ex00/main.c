821e1ae5372c5dbbfd088fd356177aa34a1d6712
Marius Nouchet <mnouchet@e2r8p10.clusters.42paris.fr>
2022-09-21 21:44:20 +0200

    Add comparaison with real func on ex00

--- c02/ex00/main.c
+++ c02/ex00/main.c
@@ -6,19 +6,22 @@
 /*   By: mnouchet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 18:08:20 by mnouchet          #+#    #+#             */
-/*   Updated: 2022/09/19 19:02:34 by mnouchet         ###   ########.fr       */
+/*   Updated: 2022/09/21 21:27:24 by mnouchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <string.h>
 
 char	*ft_strcpy(char *dest, char *src);
 
 int	main(void)
 {
 	char	src[] = "This sentence should be copied";
-	char	dest[] = "";
+	char	dest[7];
+	char	destR[7];
 
 	printf("Original: %s\n", src);	
-	printf("Copied: %s\n", ft_strcpy(dest, src));
+	printf("Copied ft: %s\n", ft_strcpy(dest, src));
+	printf("Copied real: %s\n", strcpy(destR, src));
 }
